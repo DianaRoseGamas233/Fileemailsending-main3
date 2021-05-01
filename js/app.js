aeeb63baabfb4c9f1e26479aac1c710748ca6a8a
flynndianagamas07@gmail.com <flyndianagamas07@gmail.com>
2021-05-01 14:21:00 +0700

    kicking off the project

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// Variables
+const sendBtn = document.getElementById('sendBtn'),
+email = document.getElementById('email'),
+subject = document.getElementById('subject'),
+message = document.getElementById('message'),
+reset = document.getElementById('resetbtn'),
+sendEmailForm = document.getElementById('email-form');
+
+
+
+
+
+
+// Event Listeners
+eventListeners();
+
+
+function eventListeners(){
+// App init
+document.addEventListener('DOMContentLoaded', appInit);
+
+
