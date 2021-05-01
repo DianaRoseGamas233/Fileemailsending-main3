23cc06470901298bacbdc900615a68df1ae05684
flynndianagamas07@gmail.com <flyndianagamas07@gmail.com>
2021-05-01 14:22:52 +0700

    validating fields part 1

--- js/app.js
+++ js/app.js
@@ -19,4 +19,28 @@ function eventListeners(){
 // App init
 document.addEventListener('DOMContentLoaded', appInit);
 
+  // Validate the forms
+  email.addEventListener('blur', validateField);
+  subject.addEventListener('blur', validateField);
+  message.addEventListener('blur', validateField);
 
+
+  // send email and reset button
+  sendEmailForm.addEventListener('submit', sendEmail);
+  resetBtn.addEventListener('click', resetForm);
+}
+
+
+
+
+
+// Functions
+// App intitialization
+function appInit() {
+  // disable the send button on load
+  sendBtn.disabled = true;
+}
+
+
+function sendEmail(e) {
+  e.preventDefault();
